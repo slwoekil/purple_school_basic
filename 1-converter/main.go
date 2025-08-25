6323ed85d65261b595bb6c5bda4fdd3c310abbca
asherstnev <artem.sherstnev@x5.ru>
2025-08-25 18:40:27 +0200

    добавлены счиитывание ввода и пустая функция расчета

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -8,3 +8,19 @@ func main() {
 	EURtoRUB := USDtoRUB / USDtoEUR
 	fmt.Printf("%.2f", EURtoRUB)
 }
+func getCurrencies() {
+	var currencyToChange string
+	var currencyToGet string
+	var countCurrency float64
+
+	fmt.Println("Какую валюту вы хотите обменять?")
+	fmt.Scan(currencyToChange)
+	fmt.Println("Какую валюту вы хотите получить?")
+	fmt.Scan(currencyToGet)
+	fmt.Println("Какое количество валюты вы хотите обменять?")
+	fmt.Scan(countCurrency)
+}
+
+func calculateChangedCurrency(currencyToChange string, currencyToGet string, countCurrency float64) {
+
+}
