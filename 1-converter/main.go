3cd97a96e92da5303f75ca55f67cbee4971ebc8b
asherstnev <artem.sherstnev@x5.ru>
2025-08-30 19:21:32 +0200

    Реализован конвертер валют

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,26 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const USDtoRUB = 80.70
+const USDtoEUR = 0.86
+const EURtoRUB = USDtoRUB / USDtoEUR
 
 func main() {
-	const USDtoRUB = 80.70
-	const USDtoEUR = 0.86
-	EURtoRUB := USDtoRUB / USDtoEUR
-	fmt.Printf("%.2f", EURtoRUB)
+	currencyForChange := getCurrencyForChange()
+	countCurrency := getCountOfCurrency()
+	receivedCurrency := getReceivedCurrency()
+	calculateChangedCurrency(currencyForChange, receivedCurrency, countCurrency)
 }
-func getCurrencies() {
-	var currencyToChange string
-	var currencyToGet string
+
+func calculateChangedCurrency(currencyForChange string, receivedCurrency string, countCurrency float64) {
+	var exchange string
+
+	switch {
+	case currencyForChange == "EUR" && receivedCurrency == "USD":
+		exchange = fmt.Sprintf("Сумма к получению: %.2f", countCurrency/USDtoEUR)
+		fmt.Println(exchange, receivedCurrency)
+	case currencyForChange == "EUR" && receivedCurrency == "RUB":
+		exchange = fmt.Sprintf("Сумма к получению: %.2f", EURtoRUB*countCurrency)
+		fmt.Println(exchange, receivedCurrency)
+	case currencyForChange == "EUR" && receivedCurrency == "EUR":
+		fmt.Println("Вы выбрали две одинаковые валюты")
+	case currencyForChange == "USD" && receivedCurrency == "EUR":
+		exchange = fmt.Sprintf("Сумма к получению: %.2f", USDtoEUR*countCurrency)
+		fmt.Println(exchange, receivedCurrency)
+	case currencyForChange == "USD" && receivedCurrency == "RUB":
+		exchange = fmt.Sprintf("Сумма к получению: %.2f", USDtoRUB*countCurrency)
+		fmt.Println(exchange, receivedCurrency)
+	case currencyForChange == "USD" && receivedCurrency == "USD":
+		fmt.Println("Вы выбрали две одинаковые валюты")
+	case currencyForChange == "RUB" && receivedCurrency == "EUR":
+		exchange = fmt.Sprintf("Сумма к получению: %.2f", countCurrency/EURtoRUB)
+		fmt.Println(exchange, receivedCurrency)
+	case currencyForChange == "RUB" && receivedCurrency == "USD":
+		exchange = fmt.Sprintf("Сумма к получению: %.2f", countCurrency/USDtoRUB)
+		fmt.Println(exchange, receivedCurrency)
+	case currencyForChange == "RUB" && receivedCurrency == "RUB":
+		fmt.Println("Вы выбрали две одинаковые валюты")
+	}
+}
+
+func getCurrencyForChange() string {
+	var currencyForChange string
+
+	for {
+		fmt.Println("Какую валюту вы хотите обменять? (USD, EUR, RUB)")
+		fmt.Scan(&currencyForChange)
+
+		if currencyForChange == "USD" || currencyForChange == "EUR" || currencyForChange == "RUB" {
+			break
+		}
+
+		fmt.Println("Валюта не найдена")
+	}
+
+	return currencyForChange
+}
+
+func getCountOfCurrency() float64 {
 	var countCurrency float64
 
-	fmt.Println("Какую валюту вы хотите обменять?")
-	fmt.Scan(currencyToChange)
-	fmt.Println("Какую валюту вы хотите получить?")
-	fmt.Scan(currencyToGet)
-	fmt.Println("Какое количество валюты вы хотите обменять?")
-	fmt.Scan(countCurrency)
+	for {
+		fmt.Println("Какое количество валюты вы хотите обменять?")
+
+		_, err := fmt.Scan(&countCurrency)
+
+		if err != nil {
+			fmt.Println("Вы ввели некорректное значение. Для продолжения введите число")
+			var discard string
+			fmt.Scan(&discard)
+			continue
+		}
+
+		if countCurrency > 0 {
+			break
+		}
+
+		fmt.Println("Сумма для обмена должна быть больше 0")
+	}
+
+	return countCurrency
 }
 
-func calculateChangedCurrency(currencyToChange string, currencyToGet string, countCurrency float64) {
+func getReceivedCurrency() string {
+	var receivedCurrency string
 
+	for {
+		fmt.Println("Какую валюту вы хотите получить? (USD, EUR, RUB)")
+		fmt.Scan(&receivedCurrency)
+
+		if receivedCurrency == "USD" || receivedCurrency == "EUR" || receivedCurrency == "RUB" {
+			break
+		}
+
+		fmt.Println("Валюта не найдена")
+	}
+
+	return receivedCurrency
 }
